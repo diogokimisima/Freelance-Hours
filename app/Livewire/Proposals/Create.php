c8538e1b3b242cb5886142d94678cccb0be6809f
dkimisima@gmail.com <dkimisima@gmail.com>
2024-10-13 14:47:00 -0300

    creation notification in the mail using make notification

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -5,6 +5,8 @@ namespace App\Livewire\Proposals;
 use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
+use App\Notifications\NewProposal;
+use App\Notifications\PerdeuMane;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
@@ -45,11 +47,13 @@ class Create extends Component
                 );
 
             $this->arrangePositions($proposal);
-
-            $this->dispatch('proposal::created');
-
-            $this->modal = false;
         });
+
+        $this->project->author->notify(new NewProposal($this->project));
+
+        $this->dispatch('proposal::created');
+
+        $this->modal = false;
     }
 
     public function arrangePositions(Proposal $proposal)
@@ -65,7 +69,9 @@ class Create extends Component
 
         if ($otherProposal) {
             $proposal->update(['position_status' => 'up']);
-            Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
+            $oProposal = Proposal::find($otherProposal->id);
+            $oProposal->update(['position_status' => 'down']);
+            $oProposal->notify(new PerdeuMane($this->project));
         }
 
         ArrangePositions::run($proposal->project_id);
